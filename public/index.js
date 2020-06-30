53e33a03582b9ccbdb34bba647d979905cc5d820
rodrigodiasnoronha <rodrigonoronha09@gmail.com>
2020-06-30 14:51:57 -0300

    feat: vaso de planta 3d added

--- public/index.js
+++ public/index.js
@@ -1,73 +1,73 @@
-import * as THREE from "/threejs/three.module.js";
-import { STLLoader } from "/threejs/STLLoader.js";
-import { OrbitControls } from "/threejs/OrbitControls.js";
+import * as THREE from '/threejs/three.module.js';
+import { STLLoader } from '/threejs/STLLoader.js';
+import { OrbitControls } from '/threejs/OrbitControls.js';
+import { GLTFLoader } from './threejs/GLTFLoader.js';
 
 let scene, camera, renderer, container, controls, object;
 
-let loader = new STLLoader();
-loader.load("/models/California office chair.stl", (geometry) => {
-  let material = new THREE.MeshLambertMaterial({ color: "#e02" });
-  object = new THREE.Mesh(geometry, material);
-  object.scale.set(0.08, 0.08, 0.08);
-  object.position.set(0, 35, 0);
+let loader = new GLTFLoader();
+loader.load('/models/pot/scene.gltf', (gltf) => {
+    let material = new THREE.MeshLambertMaterial({ color: '#e02' });
+    const root = gltf.scene;
 
-  init();
+    init();
+    scene.add(root);
 });
 
 function init() {
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color("#333");
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color('#333');
 
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight
-  );
-  camera.position.set(-40, 60, 100);
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight
+    );
+    camera.position.set(-40, 600, 100);
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-  container = document.getElementById("container");
-  container.appendChild(renderer.domElement);
+    container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
 
-  var helper = new THREE.GridHelper(1200, 60, 0xff3c4a, 0x404040);
-  scene.add(helper);
+    var helper = new THREE.GridHelper(1200, 60, 0xff3c4a, 0x404040);
+    scene.add(helper);
 
-  var dirLight = new THREE.DirectionalLight(0xffffff);
-  dirLight.position.set(0, 50, 100);
-  scene.add(dirLight);
+    var dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(0, 50, 100);
+    scene.add(dirLight);
 
-  scene.add(object);
+    scene.add(object);
 
-  controls = new OrbitControls(camera, renderer.domElement);
+    controls = new OrbitControls(camera, renderer.domElement);
 
-  animate();
-  window.addEventListener("resize", onWindowResize, false);
+    animate();
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 const socket = io();
 
 function animate() {
-  requestAnimationFrame(animate);
-  socket.on("gyr", (data) => {
-    let ang = data.split(" ");
-    document.getElementById("angles").innerText =
-      "X: " + ang[1] + " Y: " + ang[2] + " Z: " + ang[3];
+    requestAnimationFrame(animate);
+    socket.on('gyr', (data) => {
+        let ang = data.split(' ');
+        document.getElementById('angles').innerText =
+            'X: ' + ang[1] + ' Y: ' + ang[2] + ' Z: ' + ang[3];
 
-    object.rotation.set(
-      (parseInt(ang[1], 10) * Math.PI) / 180,
-      (parseInt(ang[3], 10) * Math.PI) / 180,
-      (parseInt(ang[2], 10) * Math.PI) / 180
-    );
-    //console.log((parseInt(ang[1], 10) * Math.PI) / 180);
-  });
-  renderer.render(scene, camera);
+        object.rotation.set(
+            (parseInt(ang[1], 10) * Math.PI) / 180,
+            (parseInt(ang[3], 10) * Math.PI) / 180,
+            (parseInt(ang[2], 10) * Math.PI) / 180
+        );
+        //console.log((parseInt(ang[1], 10) * Math.PI) / 180);
+    });
+    renderer.render(scene, camera);
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
 
-  camera.updateProjectionMatrix();
+    camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
